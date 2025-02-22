544e06244e4571a1099efb3b7f51828b9fd9093a
YassineSalihi <y.salihi7457@uca.ac.ma>
2025-02-22 10:53:34 +0100

    last

---
+++ TstString.java
@@ -0,0 +1,9 @@
+public class TestString{
+	public static void main(String[] args){
+		char c[] = {'A', 'B', 'C', 'D'};
+		System.out.println(c);
+		String str1 = new String(c);
+		System.out.print(str1);
+
+	}
+}
---
+++ ex2Correction.java
@@ -0,0 +1,14 @@
+public class PairSum {
+	public static void main(String[] args){
+		int[] array = {1, 4, 6, 2, 3, 5, 7};
+		int target = 7 //whatever number; 7 is a lucky number 
+		
+		list = new arrayList
+
+
+
+
+
+
+	}
+}
